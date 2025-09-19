bc8c7d87eb42fa5f04c5316ee512d62990d32e46
Patruxs <Patruxs@users.noreply.github.com>
2025-09-19 10:13:58 +0700

    feat: scaffold config module

---
+++ src/content/core/config.js
@@ -0,0 +1,17 @@
+((global) => {
+  const STUDOCU = (global.__STUDOCU__ = global.__STUDOCU__ || {});
+
+  STUDOCU.config = Object.freeze({
+
+
+    SCALE_FACTOR: 4,
+
+
+    HEIGHT_SCALE_DIVISOR: 4,
+
+
+    WIDTH_SCALE_DIVISOR: 4,
+
+
+  });
+})(window);
