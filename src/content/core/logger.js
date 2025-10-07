2565601826ac224cb6c29341e6d52648bef68c57
Patruxs <Patruxs@users.noreply.github.com>
2025-10-07 15:21:00 +0700

    feat: scaffold logger module

---
+++ src/content/core/logger.js
@@ -0,0 +1,5 @@
+((global) => {
+  const STUDOCU = (global.__STUDOCU__ = global.__STUDOCU__ || {});
+  const cfg = STUDOCU.config || {};
+  const PREFIX = cfg.LOG_PREFIX || "[StudocuHelper]";
+})(window);
