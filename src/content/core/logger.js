a29855b49deb2b5a3ec981eb923e3ca2025518d8
Patruxs <Patruxs@users.noreply.github.com>
2025-10-07 20:20:10 +0700

    feat: add levels in logger

--- src/content/core/logger.js
+++ src/content/core/logger.js
@@ -2,4 +2,9 @@
   const STUDOCU = (global.__STUDOCU__ = global.__STUDOCU__ || {});
   const cfg = STUDOCU.config || {};
   const PREFIX = cfg.LOG_PREFIX || "[StudocuHelper]";
+  const LEVEL = cfg.LOG_LEVEL ?? 0;
+
+  const LEVELS = { debug: 0, info: 1, warn: 2, error: 3 };
+
+
 })(window);
