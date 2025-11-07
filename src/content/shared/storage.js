bce499e8ac0ef7767667bcd40f339f81a1deaf7e
Patruxs <Patruxs@users.noreply.github.com>
2025-11-07 09:32:52 +0700

    feat: scaffold storage module

---
+++ src/content/shared/storage.js
@@ -0,0 +1,47 @@
+((global) => {
+  const STUDOCU = (global.__STUDOCU__ = global.__STUDOCU__ || {});
+
+  const hasChromeStorage =
+    typeof chrome !== "undefined" &&
+    chrome.storage &&
+    chrome.storage.sync;
+
+  STUDOCU.storage = {
+
+    get(keys) {
+      if (!hasChromeStorage) {
+        return Promise.resolve({});
+      }
+      return new Promise((resolve) => {
+        chrome.storage.sync.get(keys, (data) => {
+
+          resolve(data || {});
+        });
+      });
+    },
+
+
+    set(items) {
+      if (!hasChromeStorage) {
+        return Promise.resolve();
+      }
+      return new Promise((resolve, reject) => {
+        chrome.storage.sync.set(items, () => {
+          if (chrome.runtime.lastError) {
+            reject(chrome.runtime.lastError);
+          } else {
+            resolve();
+          }
+        });
+      });
+    },
+
+
+    async getValue(key, defaultValue = undefined) {
+      const data = await this.get(key);
+      return key in data ? data[key] : defaultValue;
+    },
+
+
+  };
+})(window);
