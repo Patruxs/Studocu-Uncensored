0472da3fa33e98e796b41ba497a7deb17dad612d
Patruxs <Patruxs@users.noreply.github.com>
2025-12-24 15:21:36 +0700

    feat: wire up normal props in style-cloner

--- src/content/shared/style-cloner.js
+++ src/content/shared/style-cloner.js
@@ -2,4 +2,31 @@
   const STUDOCU = (global.__STUDOCU__ = global.__STUDOCU__ || {});
   const S = STUDOCU.selectors || {};
   const dom = STUDOCU.dom || {};
+  const cfg = STUDOCU.config || {};
+
+
+  const NORMAL_PROPS = Object.freeze([
+    "position",
+    "left",
+    "top",
+    "bottom",
+    "right",
+    "font-family",
+    "font-weight",
+    "font-style",
+    "color",
+    "background-color",
+    "text-align",
+    "white-space",
+    "display",
+    "visibility",
+    "opacity",
+    "z-index",
+    "text-shadow",
+    "unicode-bidi",
+    "font-feature-settings",
+    "padding",
+  ]);
+
+
 })(window);
